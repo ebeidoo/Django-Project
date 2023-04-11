2a363c6cfb7bdf4d0012080756158ca57ea41a0c
Mohamed Ebeid <mohamedhassan4303@gmail.com>
2023-04-11 15:15:07 +0200

    first commit

---
+++ TicketBooking/tb_app/views.py
@@ -0,0 +1,111 @@
+from django.shortcuts import render
+#MainPage
+def MainPage (request):
+   return render (request,'tb_app/MainPage.html')
+
+#LoginPage  
+
+def LoginPage (request):
+   return render (request,'tb_app/LoginPage.html') 
+
+#Payment   
+
+def Payment (request):
+   return render (request,'tb_app/Payment.html')
+
+#NFL
+def NFLMainPage (request):
+   return render (request,'tb_app/NFLMainPage.html')
+
+def TicketPageNFL (request):
+   return render (request,'tb_app/TicketPageNFL.html')
+   
+def NFLTicket1 (request):
+   return render (request,'tb_app/NFL Ticket 1.html')   
+
+def NFLTicket2 (request):
+   return render (request,'tb_app/NFL Ticket 2.html') 
+
+def NFLTicket3 (request):
+   return render (request,'tb_app/NFL Ticket 3.html') 
+
+def NFLTicket4 (request):
+   return render (request,'tb_app/NFL Ticket 4.html') 
+
+def NFLTicket5 (request):
+   return render (request,'tb_app/NFL Ticket 5.html') 
+
+def NFLTicket6 (request):
+   return render (request,'tb_app/NFL Ticket 6.html') 
+      
+
+#MLB
+def MLBMainPage (request):
+   return render (request,'tb_app/MLBMainPage.html')    
+
+def MLBTicket1 (request):
+   return render (request,'tb_app/MLB Ticket 1.html') 
+
+def MLBTicket2 (request):
+   return render (request,'tb_app/MLB Ticket 2.html')    
+
+def MLBTicket3 (request):
+   return render (request,'tb_app/MLB Ticket 3.html') 
+
+def MLBTicket4 (request):
+   return render (request,'tb_app/MLB Ticket 4.html') 
+
+def MLBTicket5 (request):
+   return render (request,'tb_app/MLB Ticket 5.html')
+
+def MLBTicket6 (request):
+   return render (request,'tb_app/MLB Ticket 6.html')  
+
+ 
+ #Tennis
+def TennisMainPage (request):
+   return render (request,'tb_app/TennisMainPage.html')
+
+def TennisTicket1 (request):
+   return render (request,'tb_app/Tennis Ticket 1.html')
+
+def TennisTicket2 (request):
+   return render (request,'tb_app/Tennis Ticket 2.html')
+
+def TennisTicket3 (request):
+   return render (request,'tb_app/Tennis Ticket 3.html')
+
+def TennisTicket4 (request):
+   return render (request,'tb_app/Tennis Ticket 4.html')
+
+def TennisTicket5 (request):
+   return render (request,'tb_app/Tennis Ticket 5.html')
+
+def TennisTicket6 (request):
+   return render (request,'tb_app/Tennis Ticket 6.html')
+
+
+#NBA
+def NBAMainPage (request):
+   return render (request,'tb_app/NBAMainPage.html')
+
+def NBATicket1 (request):
+   return render (request,'tb_app/NBA Ticket 1.html')
+
+def NBATicket2 (request):
+   return render (request,'tb_app/NBA Ticket 2.html')
+
+def NBATicket3 (request):
+   return render (request,'tb_app/NBA Ticket 3.html')
+
+def NBATicket4 (request):
+   return render (request,'tb_app/NBA Ticket 4.html')
+
+def NBATicket5 (request):
+   return render (request,'tb_app/NBA Ticket 5.html')
+
+def NBATicket6 (request):
+   return render (request,'tb_app/NBA Ticket 6.html')
+   
+
+                   
